b05a5f297e1bc4883e232bf89fa7ce93a89c2838
Charlie Pye <charl@MacBook-Air.local>
2026-05-20 17:37:41 +0800

    added more quotes

--- src/quotes.js
+++ src/quotes.js
@@ -19,7 +19,31 @@ export const BOOK_A = {
     },
     {
       id: "a4",
-      text: "If you saw Atlassdfsdf",
+      text: "Run for your life from any man who tells you that money is evil. That sentence is the leper's bell of an approaching looter.",
+    },
+    {
+      id: "a5",
+      text: "Do you know the hallmark of a second rater? It's resentment of another man's achievement. Those touchy mediocrities who sit trembling lest someone's work prove greater than their own - they have no inkling of the loneliness that comes when you reach the top. The loneliness for an equal - for a mind to respect and an achievement to admire.",
+    },
+    {
+      id: "a6",
+      text: "Productiveness is your acceptance of morality, your recognition of the fact that you choose to live--that productive work is the process by which man's consciousness controls his existence, a constant process of acquiring knowledge and shaping matter to fit one's purpose, of translating an idea into physical form, of remaking the earth in the image of one's values",
+    },
+    {
+      id: "a7",
+      text: "Power-lust is a weed that grows only in the vacant lots of an abandoned mind. ",
+    },
+    {
+      id: "a8",
+      text: "We are on strike, we, the men of the mind. We are on strike against self-immolation. We are on strike against the creed of unearned rewards and unrewarded duties. We are on strike against the dogma that the pursuit of one's happiness is evil. We are on strike against the doctrine that life is guilt.",
+    },
+    {
+      id: "a9",
+      text: "Money is the material shape of the principle that men who wish to deal with one another must deal by trade and give value for value. Money is not the tool of the moochers, who claim your product by tears, or of the looters, who take it from you by force. Money is made possible only by the men who produce.",
+    },
+    {
+      id: "a10",
+      text: "Until and unless you discover that money is the root of all good, you ask for your own destruction. When money ceases to become the means by which men deal with one another, then men become the tools of other men. Blood, whips and guns--or dollars. Take your choice--there is no other.",
     },
   ],
 };
@@ -37,6 +61,34 @@ export const BOOK_B = {
     {
       id: "b3",
       text: "Modern industry has established the world market, for which the discovery of America paved the way. This market has given an immense development to commerce, to navigation, to communication by land. This development has, in its turn, reacted on the extension of industry; and in proportion as industry, commerce, navigation, railways extended, in the same proportion the bourgeoisie developed, increased its capital, and pushed into the background every class handed down from the Middle Ages.",
+    },
+    {
+      id: "b4",
+      text: "These labourers, who must sell themselves piece-meal, are a commodity, like every other article of commerce, and are consequently exposed to all the vicissitudes of competition, to all the fluctuations of the market.",
+    },
+    {
+      id: "b5",
+      text: "In short, the Communists everywhere support every revolutionary movement against the existing social and political order of things. In all these movements they bring to the front, as the leading question in each, the property question, no matter what its degree of development at the time.",
+    },
+    {
+      id: "b6",
+      text: "Free trade: for the benefit of the working class. Protective duties: for the benefit of the working class. Prison Reform: for the benefit of the working class. This is the last word and the only seriously meant word of bourgeois Socialism. It is summed up in the phrase: the bourgeois is a bourgeois—for the benefit of the working class.",
+    },
+    {
+      id: "b7",
+      text: "At this stage, therefore, the proletarians do not fight their enemies, but the enemies of their enemies, the remnants of absolute monarchy, the landowners, the non-industrial bourgeois, the petty bourgeois. Thus, the whole historical movement is concentrated in the hands of the bourgeoisie; every victory so obtained is a victory for the bourgeoisie.",
+    },
+    {
+      id: "b8",
+      text: "The average price of wage-labor, is the minimum wage, i.e., that quantum of the means of subsistence which is absolutely requisite to keep the laborer in bare existence as a laborer.  What, therefore, the wage-laborer appropriates by means of his labor, merely suffices to prolong and reproduce a bare existence.",
+    },
+    {
+      id: "b9",
+      text: "And here it becomes evident, that the bourgeoisie is unfit any longer to be the ruling class in society, and to impose its conditions of existence upon society as an overriding law.  It is unfit to rule because it is incompetent to assure an existence to its slave within his slavery, because it cannot help letting him sink into such a state, that it has to feed him, instead of being fed by him.  Society can no longer live under this bourgeoisie, in other words, its existence is no longer compatible with society.",
+    },
+    {
+      id: "b10",
+      text: "Modern industry has converted the little workshop of the patriarchal master into the great factory of the industrial capitalist. Masses of labourers, crowded into the factory, are organised like soldiers. As privates of the industrial army they are placed under the command of a perfect hierarchy of officers and sergeants. Not only are they slaves of the bourgeois class, and of the bourgeois State; they are daily and hourly enslaved by the machine, by the over-looker, and, above all, by the individual bourgeois manufacturer himself. The more openly this despotism proclaims gain to be its end and aim, the more petty, the more hateful and the more embittering it is.",
     },
   ],
 };
